c1b88e4741b5acebd23cce06f33751c5e8a7c668
KennRobot <kebrgonzalezdo@ittepic.edu.mx>
2025-05-12 18:59:37 -0700

    Upload project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const cds = require('@sap/cds'); 
+const cors = require('cors') 
+const router = express.Router(); 
+module.exports = async (o) => { 
+    let app = express(); 
+    app.express = express; 
+    try { 
+    app.use(express.json({limit:'500kb'})); 
+    app.use(cors()); 
+    app.use('/api', router); 
+    app.get('/', (req, res) => { 
+    res.end('SAP CDS está en ejecución...${req.url}'); 
+
+    }); 
+    o.app = app; 
+    o.app.httpServer = await cds.server(o); 
+    } catch (error) { 
+        console.error('Error starting server:', error); 
+        process.exit(1); 
+    } 
+    return o.app.httpServer; 
+};
